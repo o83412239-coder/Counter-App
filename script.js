f3a18579045748df363799eb192417918a4de11b
o83412239-coder <o83412239@gmail.com>
2026-03-16 16:44:57 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let count=0;
+
+function increase(){
+count++;
+document.getElementById("count").textContent=count;
+}
+
+function decrease(){
+count--;
+document.getElementById("count").textContent=count;
+}
